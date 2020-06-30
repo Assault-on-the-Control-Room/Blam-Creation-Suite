361f98e87c137eb35ca0bc09aa4d76d30941898c
matty45 <matthewtoo5@googlemail.com>
2020-06-30 22:39:15 +0100

    [Reach] chud_definitions.cpp - Blank Slate

--- Tags/TagDefinitions/blofeld/interface/chud/chud_definitions.cpp
+++ Tags/TagDefinitions/blofeld/interface/chud/chud_definitions.cpp
@@ -8,6 +8,94 @@ namespace blofeld
 	TAG_REFERENCE(chud_reference, CHUD_TAG);
 	TAG_REFERENCE(chud_globals_reference, CHUD_GLOBALS_TAG);
 
+	TAG_BLOCK(unknown_block_1, 65536)
+	{
+		{_field_real, "unknown"},
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_long_integer, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_terminator }
+	};
+
+	TAG_BLOCK(unknown_block_2, 65536)
+	{
+		{_field_real, "unknown"},
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_terminator }
+	};
+	
 	TAG_GROUP(chud_animation, CHUD_ANIMATION_TAG)
 	{
 		{ _field_terminator }
@@ -15,6 +103,12 @@ namespace blofeld
 
 	TAG_GROUP(chud, CHUD_TAG)
 	{
+		{_field_block, "unknown block 1", &unknown_block_1_block },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_real, "unknown" },
+		{ _field_block, "unknown block 2", &unknown_block_2_block },
 		{ _field_terminator }
 	};
 
@@ -22,6 +116,7 @@ namespace blofeld
 	{
 		{ _field_terminator }
 	};
+
 
 }
 
