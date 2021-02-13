07ec1cf8233e1b2b0569a4d157b08ebaaf6550f1
Akarias <Akarias@users.noreply.github.com>
2021-02-13 17:51:30 +0000

    Update cache_file_builder_tag_resource_manager.cpp
    whups

--- Tags/TagDefinitions/macaque/cache/cache_file_builder_tag_resource_manager.cpp
+++ Tags/TagDefinitions/macaque/cache/cache_file_builder_tag_resource_manager.cpp
@@ -21,6 +21,11 @@ namespace macaque
 		{ _field_block, "shared files", &cache_file_shared_file_block },
 		{ _field_block, "file pages", &cache_file_resource_file_page_block, _field_id_wide },
 		{ _field_block, "streaming subpage tables", &cache_file_resource_streaming_subpage_table_block },
+
+		{ _field_legacy, _field_version_platform_include, _platform_type_pc, 2 },
+		{ _field_legacy, _field_version_less_or_equal, _engine_type_haloreach, 1 },
+		{ _field_legacy, _field_block, "unknown@", &g_null_block },
+
 		{ _field_block, "sections", &cache_file_resource_section_block },
 		{ _field_long_integer, "required location count" },
 		{ _field_long_integer, "required dvd location count" },
